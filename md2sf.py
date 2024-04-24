26df0b7b88ffe060dbeb6406048cbaf6cf9a9eec
David A. Desrosiers <setuid@gmail.com>
2024-04-23 21:49:02 -0400

    Pushing local changes to address mistune updates, clearer docstrings, better base64 handling, lazy-load magic module and other small fixes

--- md2sf.py
+++ md2sf.py
@@ -1,65 +1,66 @@
 #!/usr/bin/env python3
 
-
+import base64
 import os
 
 import click
 import mistune
-from mistune.scanner import escape
-import base64
-import magic
+from mistune import escape
+
 
 class sf_html_render(mistune.HTMLRenderer):
     # This is a custom mistune extension that will override the default
     # behavior of several inline block element handlers to allow them to emit
     # the HTML needed by Saleforce's Knowledge system
+    """Custom renderer for converting Markdown to Salesforce's Knowledge system HTML format."""
 
     def codespan(self, text):
-        """override the default `code` block handler"""
+        """Override for `code` span with specific style."""
         return '<code style="font-size:1em;color:#00f;">' + escape(text) + "</code>"
 
     def paragraph(self, text):
-        """override the default paragraph handler"""
+        """Override for paragraph to maintain standard HTML paragraph tagging."""
         return "<p>" + text + "</p>\n"
 
     def image(self, url, alt, title=None):
-        """override the default image handler to base64 encode the
-           file content, if the file is available"""
-        
+        """Override for images to handle base64 encoding if the image file exists."""
         if os.path.isfile(url):
             with open(url, "rb") as image_file:
-                encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-
-            mimetype = magic.from_file(url, mime=True)
-            url = f'data:{mimetype};base64,{encoded_string}'
+                encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+                mimetype = self._get_mime_type(url)
+                url = f"data:{mimetype};base64,{encoded_string}"
 
         return f'<img alt="{alt}" title="{title}" src="{url}" style="margin-top: 5px; margin-bottom: 5px;" />'
 
     def heading(self, text, level, **attrs):
-        """Our KB template does not like headers, so converting them just to a bold text
-           First Heading should not have a margin-top"""
-
-        return f"<p style='margin-top: 15px;'><b>{text}</b></p>\n"
+        """Override for headings to convert them into bold text paragraphs as per what SF needs."""
+        margin_style = "margin-top: 15px;" if level == 1 else ""
+        return f'<p style="{margin_style}"><b>{text}</b></p>\n'
 
     def block_code(self, code, info=None):
-        """inject the correct `code` block handlers for styling"""
-        html = '<pre class="ckeditor_codeblock"'
-        if info is not None:
-            info = info.strip()
-        return html + ">" + escape(code) + "</pre>\n"
+        """Override for `code` blocks to use custom SF page styles."""
+        info = info.strip() if info is not None else ""
+        return '<pre class="ckeditor_codeblock">' + escape(code) + "</pre>\n"
+
+    def _get_mime_type(self, url):
+        """Lazy-load magic here since it's only used once, faster import loading"""
+        import magic
+
+        return magic.from_file(url, mime=True)
 
 
 @click.command(help="Plese supply the path to a KB article in Markdown format to parse")
 @click.argument("filename", type=click.Path(exists=True, readable=True), nargs=1)
 def main(filename):
-
-    with open(filename) as sf_kb:
+    """Main function to parse a Markdown file and save it as HTML."""
+    with open(filename, "r") as sf_kb:
         sf_kb_file = sf_kb.read()
 
     markdown = mistune.create_markdown(renderer=sf_html_render())
+    html_filename = os.path.splitext(filename)[0] + ".html"
 
-    sf_html = open(os.path.splitext(filename)[0] + ".html", "w")
-    sf_html.write(markdown(sf_kb_file))
+    with open(html_filename, "w") as sf_html:
+        sf_html.write(markdown(sf_kb_file))
 
 
 if __name__ == "__main__":
